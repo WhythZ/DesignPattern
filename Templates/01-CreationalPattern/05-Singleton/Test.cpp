702a31692729525eabe9cc41a3a88b781203029e
WhythZ <jichenzong@outlook.com>
2024-09-17 15:45:01 +0800

    9.17 about singleton memory

--- Templates/01-CreationalPattern/05-Singleton/Test.cpp
+++ Templates/01-CreationalPattern/05-Singleton/Test.cpp
@@ -1,8 +1,9 @@
 #include "GameManager.hpp"
 
 int main()
-{
-	//运行游戏主管理器内的主循环方法，并获取返回值
+{   
+    //运行游戏主管理器内的主循环方法，并获取返回值
+    //注意这里获取的实例是开辟在堆区的，但此处可以不用delete，系统会给你擦屁股（虽说最好处理一下，但是单例模式问题不大）
     return GameManager::GetInstance()->Run();
     //GamaManager Run Success
 }
